2b6b5c0dacd6f60cf16eadfb4642ecea1b4adb33
igordidier <46652986+igordidier@users.noreply.github.com>
2019-12-10 10:33:50 +0100

    php

--- station.php
+++ station.php
@@ -2,13 +2,14 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="main.css">
-    <title></title>
+    <link rel="stylesheet" href="station.css">
+    <title>Station</title>
+    <link rel="shortcut icon" href="fav.jpg">
   </head>
   <body>
-    <img src="ski.jpg"/>
+
     <div class="topnav">
-  <a  href="#home">Acceuil</a>
+  <a  href="index.html">Acceuil</a>
   <a href="station.php">Station</a>
   <a href="#contact">Hebergement</a>
   <a href="#about">Search</a>
