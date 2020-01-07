a0619484e3caa45c731c8d6758df5e86cbe5b1e7
dame0112 <58723018+dame0112@users.noreply.github.com>
2020-01-07 15:17:47 +0100

    Update stpierre.php

--- stpierre.php
+++ stpierre.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="station.css">
-    <title>Station de st pierre</title>
+    <title>saint pierre</title>
     <link rel="shortcut icon" href="fav.jpg">
   </head>
   <body>
@@ -21,7 +21,7 @@
 </div>
 
 <div class="texte">
-  <u>Stations saint pierre</u>
+  <u>Stations de saint Pierre</u>
 <div class="retour">
 
   <a href="station.php" style=" text-decoration: none; color: white; ">←Retour</a>
@@ -33,10 +33,38 @@
 <div class="containers">
 
 <a href="#"> <div class="container">
-    <img class="imgcont" src="img/stpierre.jpg" alt="st_pierre">
-<p style="font-size: 20px;margin: 0px;">st pierre</p>
-    <p style="margin: 0;font-size: 10px;">chartreuse</p>
+    <img class="imgcont" src="img/stpierre.jpg" alt="park saint Pierre">
+<p style="font-size: 20px;margin: 0px;">saint Pierre</p>
+    <p style="margin: 0;font-size: 10px;">Chartreuse</p>
   </div>
   </a>
 
 </div>
+
+<p class="texte_description">
+  Saint Pierre de Chartreuse, située à 1345m d’altitude en Isère, propose un domaine skiable de 35km. Nos lecteurs ont partagé 58 avis sur Saint Pierre de Chartreuse avec une note moyenne de 4,1. Pour préparer votre séjour, consultez les prévisions météo-neige, le plan des pistes, les photos live, les webcams et l’actualité de la station.
+</p>
+
+<p class="texte_description">
+Nombre de Remontées : 10.
+</p>
+
+<p class="texte_description">
+  Tarifs des differents forfaits disponibles : ↓↓↓ (Source : <a href="https://www.ski-alpin-chartreuse.com/tarifs" target="_blank"> https://www.saint_pierre.com</a>)
+</p>
+
+<div class="image">
+    <img class="image" src="img/prix_chamrousse.png" alt="Tarifs_forfait_saint Pierre">
+
+  </div>
+<p class="texte_description"> Localisation de la Station (Google Maps): </p>
+  <iframe class="carte" src="https://www.google.com/maps/d/embed?mid=19xO0W-rQ7zBh21P4TrreZdw669XrKA8k" width="1100" height="680"></iframe>
+
+
+<footer> <p>Copyright Dame Niasse, Louis Nardini, Igor Didier</p>
+  <div class="hautpage"><a href="#haut-de-page">Retour vers le Haut de la page</br></a></div>
+</footer>
+
+
+  </body>
+</html>
