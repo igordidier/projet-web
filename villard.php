e4faae22d0195f15efe33e1e6db249d7771b5242
BarKoza <BarKoza@users.noreply.github.com>
2020-01-07 11:35:01 +0100

    Ajout des Stations

---
+++ villard.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="station.css">
+    <title>Station de Villard-de-Lans</title>
+    <link rel="shortcut icon" href="fav.jpg">
+  </head>
+  <body>
+
+    <div class="topnav">
+      <h1 id ="haut-de-page"></h1>
+  <a href="index.html">Acceuil</a>
+  <a href="station.php"><b><strong><span style="text-decoration: underline;">Stations</span></strong></b></a>
+  <a href="hebergement.php">Hebergements</a>
+  <a href="#about">Search</a>
+  <div class="sign">
+<a href="#">Contacter</a>
+<a href="#">Reserver</a>
+  </div>
+</div>
+
+<div class="texte">
+  <u>Station de Villard-de-Lans</u>
+<div class="retour">
+
+  <a href="station.php" style=" text-decoration: none; color: white; ">←Retour</a>
+
+</div>
+
+</div>
+
+<div class="containers">
+
+<a href="#"> <div class="container">
+  <img class="imgcont" src="img/villard.jpg" alt="station Villard de lans">
+<p style="font-size: 20px;margin: 0px;">Villard de lans</p>
+  <p style="margin: 0;font-size: 10px;">Vercors</p>
+  </div>
+  </a>
+
+</div>
+
+<p class="texte_description">
+  Description : Le domaine skiable de Villard-de-Lans Corrençon-en-Vercors est un domaine skiable (alpin et fond) français, du massif du Vercors.
+  Il est situé sur les territoires des communes de Villard-de-Lans et Corrençon-en-Vercors, dans le département de l'Isère, en région Auvergne-Rhône-Alpes.
+  Le domaine est située dans le massif du Vercors, à proximité des stations de ski d'Autrans-Méaudre en Vercors et Lans-en-Vercors.
+  Il est situé à environ 32 km de Grenoble, 130 km de Lyon et 584 km de Paris.  </p>
+
+<p class="texte_description">
+Nombre de Remontées : 19.
+</p>
+
+<p class="texte_description">
+  Tarifs des differents forfaits disponibles : ↓↓↓ (Source : <a href="https://www.villardcorrencon.com" target="_blank"> https://www.villardcorrencon.com</a>)
+</p>
+
+<div class="image3">
+    <img class="image3" src="img/prix_villard.png" alt="Tarifs_forfait_villard">
+
+  </div>
+<p class="texte_description"> Localisation de la Station (Google Maps): </p>
+  <iframe class="carte" src="https://www.google.com/maps/d/embed?mid=1KYG1fBct06AmcEXbmNm3v6UwQi6lZ77E" width="1100" height="680"></iframe>
+
+
+<footer> <p>Copyright Dame Niasse, Louis Nardini, Igor Didier</p>
+  <div class="hautpage"><a href="#haut-de-page">Retour vers le Haut de la page</br></a></div>
+</footer>
+
+
+  </body>
+</html>
